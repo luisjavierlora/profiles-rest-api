f479d3567902b2ee0d9f57f93a27ab942798b4b5
Luis Lora <javier.lora@udea.edu.co>
2021-09-10 15:32:31 -0500

    Add user profile feed API

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
-
+from django.conf import settings
 
 # Create your models here.
 class UserProfileManager(BaseUserManager):
@@ -29,7 +29,7 @@ class UserProfileManager(BaseUserManager):
         user.is_superuser = True
 
         user.is_staff = True
-        user.save(using=self._db)
+        user.save(using=self)
 
 
 
@@ -58,3 +58,15 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
 
 
 
+class ProfileFeedItem(models.Model):
+    """Profile status update"""
+    user_profile = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return the model as a string"""
+        return self.status_text
--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -1,5 +1,6 @@
 from rest_framework import permissions
 
+
 class UpdateOwnProfile(permissions.BasePermission):
     """Allow user to edit their own profile"""
 
@@ -12,5 +13,16 @@ class UpdateOwnProfile(permissions.BasePermission):
         return obj.id == request.user.id
 
 
-    
-    
+class UpdateOwnStatus(permissions.BasePermission):
+    """Allow users to update their own status"""
+
+    def has_object_permission(self,request,view,obj):
+        """Check the user is trying to update their own satus"""
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+
+        return obj.user_profile.id == request.user.id
+
+
